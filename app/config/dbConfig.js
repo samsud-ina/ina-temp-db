01cde8755bfce0e027dd078b41da052b8c448acc
Samsud Dhuha <suddhuha@gmail.com>
2025-06-25 11:42:27 +0700

    update path ca pem

--- app/config/dbConfig.js
+++ app/config/dbConfig.js
@@ -10,7 +10,7 @@ const pool = mySql.createConnection({
   password: env.password,
   database: env.database,
   ssl: {
-    ca: fs.readFileSync(path.join(__dirname, '..', 'ca.pem'))
+    ca: fs.readFileSync(path.join(__dirname, 'ca.pem'))
   }
 });
 
