efb0532cb2063261b38c21c32a55432f74c6547c
Samsud Dhuha <suddhuha@gmail.com>
2025-06-25 11:36:03 +0700

    update path ca pem

--- app/config/dbConfig.js
+++ app/config/dbConfig.js
@@ -1,6 +1,7 @@
 const env = require('./env.js');
 const fs = require('fs');
 const mySql = require('mysql2');
+const path = require('path');
 
 const pool = mySql.createConnection({
   host: env.host,
