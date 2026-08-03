9c6618f8ec2c5a2f52b5da4b78461d13a22ba798
Samsud Dhuha <suddhuha@gmail.com>
2026-08-03 17:23:51 +0700

    using node js 24

--- functions/api.js
+++ functions/api.js
@@ -5,11 +5,10 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-
 app.use(express.json());
 
-let routerApps = require('../app/routers/router.js');
+const routerApps = require('../app/routers/router.js');
 
-app.use('/.netlify/functions/app', routerApps);
+app.use('/.netlify/functions/api', routerApps);
 
 module.exports.handler = serverless(app);
